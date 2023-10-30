3624eadb3b1034749cf84cfe23c520dd0b9c18ff
NiklasRestory <niklas.cullberg@restory.ai>
2023-10-30 09:10:41 +0100

    First Commit

---
+++ SOS23ProgrammersAndProjects30Okt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SOS23ProgrammersAndProjects30Okt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
